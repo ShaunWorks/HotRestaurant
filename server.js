4ab795147cb8aabc6253c6d22cc66ffa14ad7849
heincm <chris.m.hein@gmail.com>
2019-02-16 10:55:25 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var path = require("path");
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+//Data
+var tables = [{
+    name: "Ezra"
+}];
+var waitlist = [];
+//Routes
+app.get("/api/tables", function(req,res) {
+    return res.json(tables);
+});
+app.get("/api/waitlist", function(req,res) {
+    return res.json(waitlist);
+});
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
